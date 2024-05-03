cf56ece0f4abb0e2b3597fb24e2697fe8955582c
Ananya Gupta <anaya29003@gmail.com>
2024-05-03 18:56:30 +0530

    Setting Database

--- Backend/app.js
+++ Backend/app.js
@@ -3,11 +3,17 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
+const { db } = require("./db/db");
 
 // middlewares
 app.use(express.json());
 app.use(cors());
 
-app.listen(port, () => {
-  console.log("Listening to port:", port);
-});
+const server = async () => {
+  await db();
+  app.listen(port, () => {
+    console.log("Listening to port:", port);
+  });
+};
+
+server();
---
+++ Backend/db/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const db = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log("Database failed to connect", error);
+  }
+};
+
+module.exports = { db };
