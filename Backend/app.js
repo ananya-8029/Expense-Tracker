bce0546c4996312f5719f72f052cbe1f54037403
Ananya Gupta <anaya29003@gmail.com>
2024-05-03 18:35:43 +0530

    Setting Database

--- Backend/app.js
+++ Backend/app.js
@@ -1,4 +1,13 @@
-require("dotenv").config()
-const port = process.env.PORT
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT;
 
-console.log(port)
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => {
+  console.log("Listening to port:", port);
+});
