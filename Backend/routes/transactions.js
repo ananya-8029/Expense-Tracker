72895aa27e9c6619fcc68e0e473463dd9b0d8d16
Ananya Gupta <anaya29003@gmail.com>
2024-05-06 16:37:11 +0530

    transaction.js api call

--- Backend/routes/transactions.js
+++ Backend/routes/transactions.js
@@ -1,6 +1,12 @@
-const { addincome } = require("../controllers/income");
+const {
+  addincome,
+  getincomes,
+  deleteincome,
+} = require("../controllers/income");
 
 const router = require("express").Router();
 
 router.post("/addincome", addincome);
+router.get("/getincomes", getincomes);
+router.delete("/deleteincome/:id", deleteincome);
 module.exports = router;
