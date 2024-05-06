f618bcad090e81743ecece61f25199e78a95c2cd
Ananya Gupta <anaya29003@gmail.com>
2024-05-06 16:53:22 +0530

    transaction.js api calls for expenses

--- Backend/routes/transactions.js
+++ Backend/routes/transactions.js
@@ -1,4 +1,9 @@
 const {
+  addexpense,
+  getexpenses,
+  deleteexpense,
+} = require("../controllers/expense");
+const {
   addincome,
   getincomes,
   deleteincome,
@@ -9,4 +14,7 @@ const router = require("express").Router();
 router.post("/addincome", addincome);
 router.get("/getincomes", getincomes);
 router.delete("/deleteincome/:id", deleteincome);
+router.post("/addexpense", addexpense);
+router.get("/getexpenses", getexpenses);
+router.delete("/delete/:id", deleteexpense);
 module.exports = router;
