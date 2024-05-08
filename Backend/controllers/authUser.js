1a78d79944ffb042e21c2e9e898aedb1531bd18f
Ananya Gupta <anaya29003@gmail.com>
2024-05-08 18:19:39 +0530

    Creating Login and Register api

--- Backend/controllers/authUser.js
+++ Backend/controllers/authUser.js
@@ -2,14 +2,14 @@ const UserSchema = require("../models/UserSchema");
 require("dotenv").config();
 
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
   const { username, email, password } = req.body;
   try {
     const checkEmail = await UserSchema.findOne({ email: email });
     if (checkEmail) {
-      res.status(400).json({ message: "User already exists!" });
+      return res.status(400).json({ message: "User already exists!" });
     } else {
       const salt = await bcrypt.genSalt(10);
       const securedPassword = await bcrypt.hash(password, salt);
@@ -23,10 +23,36 @@ exports.register = async (req, res) => {
       const data = { newUser: { id: newUser.id } };
 
       const authToken = jwt.sign(data, process.env.JWT_KEY);
-      res.statue(200).json({ authToken: authToken });
+      return res.statue(200).json({ authToken: authToken });
     }
   } catch (error) {
     console.log(error.message);
-    res.status(500).json({ message: "Internal Server error" });
+    return res.status(500).json({ message: "Internal Server error" });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await UserSchema.findOne({ email });
+    if (!user) {
+      return res.status(500).json({ message: "Invalid User Credentials" });
+    }
+
+    const isPassword = await bcrypt.compare(password, user.password);
+    if (!isPassword) {
+      return res.status(500).json({ message: "Invalid User Credentials" });
+    }
+
+    const data = { user: { id: user.id } };
+
+    const signinToken = jwt.sign(data, process.env.JWT_KEY, {
+      expiresIn: 3600,
+    });
+
+    return res.status(200).json({ authToken: signinToken, user: user });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ message: "Internal Server Error" });
   }
 };
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,6 +1,7 @@
-const { register } = require("../controllers/authUser");
+const { register, login } = require("../controllers/authUser");
 
 const router = require("express").Router();
 
 router.post("/register", register);
+router.post("/login",login)
 module.exports = router;
