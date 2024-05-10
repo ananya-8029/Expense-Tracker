7e8046a1888059d0c3650f0153b2dc13ff525ed1
Ananya Gupta <anaya29003@gmail.com>
2024-05-11 00:27:58 +0530

    Updating MenuBar

--- Frontend/src/Components/Menu_Bar/MenuBar.jsx
+++ Frontend/src/Components/Menu_Bar/MenuBar.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import {
   dashBoardIcon,
   transactionIcon,
   viewExpensesIcon,
   viewIncomeIcon,
 } from "../../utils/Icons";
+import "../Menu_Bar/MenuBar.css";
 
 const MenuBar = () => {
+  const [icon, setIcon] = useState("dashBoardIcon");
+  const handleActivebtn = (iconName) => {
+    setIcon(iconName);
+  };
+
+  console.log(icon);
   return (
     <>
       <div className="h-screen w-[5vmax] flex justify-center items-center">
@@ -13,22 +21,46 @@ const MenuBar = () => {
           <div className="h-[25%] w-full flex justify-center items-center">
             <div className="h-[3vmax] w-[3vmax] rounded-full border-2 border-purple-500"></div>
           </div>
-          <div className=" flex flex-col justify-center items-center h-[45%] gap-[3vmax] border-y-[4px] w-[80%] border-[#F7F6F6]">
-            <div className="mt-[2vmax] w-full h-[20%] flex justify-center items-center cursor-pointer hover:scale-90 transition-all duration-300 hover:transition-all hover:duration-300 relative">
-              <div className="before:h-full before:w-[150%] before:absolute before:bg-[#F7F6F6] before:rounded-md before:top-0 before:left-0">
-                <span className="relative z-[2]">{dashBoardIcon}</span>
+          <div className=" flex flex-col justify-center items-center h-[45%] gap-[1.5vmax] border-y-[4px] w-[80%] border-[#F7F6F6]">
+            <div className="mt-[2vmax] w-full h-full flex justify-center items-center cursor-pointer relative">
+              <div className={`${icon == "dashBoardIcon" ? "active" : ""}`}>
+                <button
+                  onClick={() => handleActivebtn("dashBoardIcon")}
+                  className="relative z-[2] hover:scale-90 transition-all duration-300 hover:transition-all hover:duration-300"
+                >
+                  {dashBoardIcon}
+                </button>
               </div>
             </div>
-            <div className="w-full h-[20%] flex justify-center items-center cursor-pointer hover:scale-90 transition-all duration-300 hover:transition-all hover:duration-300 relative">
-              <div className="before:h-full before:w-[150%] before:absolute before:bg-[#F7F6F6] before:rounded-md before:top-0 before:left-0">
-                <span className="relative z-[2]">{transactionIcon}</span>
+            <div className="w-full h-full flex justify-center items-center cursor-pointer hover:scale-90 transition-all duration-300 hover:transition-all hover:duration-300 relative">
+              <div className={`${icon == "transactionIcon" ? "active" : ""}`}>
+                <button
+                  onClick={() => handleActivebtn("transactionIcon")}
+                  className="relative z-[2] hover:scale-90 transition-all duration-300 hover:transition-all hover:duration-300"
+                >
+                  {transactionIcon}
+                </button>
               </div>
             </div>
-            <div className="bg-orange-400 w-full h-[20%] flex justify-center items-center cursor-pointer hover:scale-90 transition-all duration-300 hover:transition-all hover:duration-300">
-              <div>{viewIncomeIcon}</div>
+            <div className="w-full h-full flex justify-center items-center cursor-pointer hover:scale-90 transition-all duration-300 hover:transition-all hover:duration-300 relative">
+              <div className={`${icon == "viewIncomeIcon" ? "active" : ""}`}>
+                <button
+                  onClick={() => handleActivebtn("viewIncomeIcon")}
+                  className="relative z-[2] hover:scale-90 transition-all duration-300 hover:transition-all hover:duration-300"
+                >
+                  {viewIncomeIcon}
+                </button>
+              </div>
             </div>
-            <div className="bg-red-400 w-full h-[20%] flex justify-center items-center cursor-pointer hover:scale-90 transition-all duration-300 hover:transition-all hover:duration-300">
-              <div>{viewExpensesIcon}</div>
+            <div className="mb-[2vmax] w-full h-full flex justify-center items-center cursor-pointer relative">
+              <div className={`${icon == "viewExpensesIcon" ? "active" : ""}`}>
+                <button
+                  onClick={() => handleActivebtn("viewExpensesIcon")}
+                  className="relative z-[2] hover:scale-90 transition-all duration-300 hover:transition-all hover:duration-300"
+                >
+                  {viewExpensesIcon}
+                </button>
+              </div>
             </div>
           </div>
         </div>
